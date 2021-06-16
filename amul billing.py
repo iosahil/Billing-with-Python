e6e4a0dd91897e117c498ad195453818ee7d45e5
Sahil Choudhary <80662496+sahil30123@users.noreply.github.com>
2021-06-17 04:34:40 +0530

    Python Code for Daily-Dairy Billing
    It's a code that first authenticates the user & then allows them to review rates and data of certain dairy stocks. Then it also empowers the user to connect with excel inside the python code instead of producing any data mayhem. With such a modest user-interactive window, the user could complete their job of taking orders from merchants of a dairy store with ease & comfort.
    It was my first-ever success code in my life. #FirstCode

---
+++ amul billing.py
@@ -0,0 +1,7 @@
+pip install pyfiglet
+import pyfiglet as ascii
+wel = pyfiglet.figlet_format("Welcome to Bill")
+print(wel)
+
+rate = {"gold500": 26.18, "shakti500": 23.18, "cow500": 22.18, "taza500": 21.18}
+
