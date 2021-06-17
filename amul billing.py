68b2e1efe96e3a63a578a19abe9712f6cd4b1254
Sahil Choudhary <80662496+sahil30123@users.noreply.github.com>
2021-06-18 01:57:09 +0530

    UPDATE  18-01-21

--- amul billing.py
+++ amul billing.py
@@ -6,9 +6,23 @@ import time
 import pyfiglet as art
 import openpyxl as excel
 
+
+class Color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
 name = str(input("\nEnter your name:"))
 name = name.capitalize()
-print('\nHi,', name.capitalize())
+print('\nHi,', Color.BOLD + name.capitalize() + Color.END)
 
 passcode = 0x75AB
 count = 0
@@ -31,9 +45,7 @@ while user_pass != passcode and count < 3:
             print('\033[1m' + 'USER LOCKED OUT!!')
             exit()
 
-print('\033[1m' + 'WELCOME TO BILLING MACHINE, ' + name.upper())
-# Use '\033[1m' to make statement bold like you use '*' in Whatsapp
-
+print(Color.BOLD + 'WELCOME TO BILLING MACHINE, ' + name.upper() + Color.END)
 
 rate = [['Gold', '500ml', 26.18],
         ['Gold', '1L', 51.35],
@@ -54,10 +66,10 @@ rate = [['Gold', '500ml', 26.18],
 
 ans1 = input('Want to know rates? [Y/N] \n')
 if (ans1 == "Y") or (ans1 == "y"):
-    print(tabulate(rate, headers=["Item", "Quantity", "Selling Price"], numalign="left"))
+    print(tabulate(rate, headers=[Color.BOLD + "Item", "Quantity", "Selling Price" + Color.END], numalign="left"))
     print('\nBahut sasta hai, \nWholesale price hai!!!! \n#Exclusively_On_Softline')
 else:
-    print('Paisa toh ped pe uggta hai na be,', name, ':)')
+    print('Okay,', name, ':)')
 
 ans2 = input('\nWant to start billing machine? [Y/N] \n')
 if (ans2 == "Y") or (ans2 == "y"):
@@ -68,65 +80,66 @@ if (ans2 == "Y") or (ans2 == "y"):
             time.sleep(0.25)
         print("Complete.\n")
     except:
-        print("\nERROR ~~ 'tqdm' package not found\nSkipping progress bar visualization\n\n")
+        print(Color.RED + "\nERROR ~~ 'tqdm' package not found\nSkipping progress bar visualization\n\n" + Color.END)
 
     # Let's create art
     try:
-
         banner = art.figlet_format("WELCOME TO SOFTLINE AMUL BILLING", font="digital")
-        print(banner)
+        print(Color.BOLD + Color.GREEN + banner + Color.END)
     except:
-        print("\nERROR ~~ 'pyfiglet' package not found\nSkipping art visualization\n\n")
+        print(Color.RED + "\nERROR ~~ 'pyfiglet' package not found\nSkipping art visualization\n\n" + Color.END)
         print('Opening Billing Machine...')
 
     # Excel Connection Starts Here!
-    print('Start Order Sheet of date:\n')
-    print("[1] Today\n"
-          "[2] Tomorrow\n"
-          "[3] Enter Date Manually\n")
+    print('Connect to EXCEL of date:\n')
+    print(Color.BOLD + "[1] Today\n"
+                       "[2] Yesterday\n"
+                       "[3] Enter Date Manually\n" + Color.END)
     ans3 = int(input("Enter 1 or 2 or 3:\n"))
+    dat = ''
     if ans3 == 1:
         dat = today = datetime.date.today().strftime("%d-%m-%Y")
     if ans3 == 2:
-        dat = tomorrow = datetime.date.today().strftime("%d-%m-%Y") + datetime.timedelta(days=1).strftime("%d-%m-%Y")
+        yesterday = datetime.date.today() - datetime.timedelta(days=1)
+        dat = yesterday.strftime("%d-%m-%Y")
     if ans3 == 3:
-        dat = manual_date = str(input("Enter Date Manually (DD-MM-YYYY):\n"))
+        dat = manual_date = str(input("\nEnter Date Manually (DD-MM-YYYY):"))
     try:
         excel_path = 'O-' + dat + '.xlsx'
         wb = excel.load_workbook("Excel Files\\" + excel_path)
     except:
-        print("ERROR\n"
-              "Can't find Excel sheet with date - " + dat + ".\n"
-                                                            "Please Enter Excel File Location Manually.")
+        print(Color.RED + "ERROR\n"
+                          "Can't find Excel sheet with date - " + dat + ".\n"
+                                                                        "Please Enter Excel File Location Manually." + Color.END)
         excel_path = input("\n\nEnter Excel file path \n(without quotes)(with double slashes):\n")
-        wb = excel.load_workbook("Excel Files\\" + excel_path)
+        wb = excel.load_workbook(excel_path)
     try:
-        print('Connected with Excel - ' + excel_path)
+        print('Connected with Excel - ' +Color.BOLD + excel_path+Color.END)
         print('Connecting with sheets inside Excel...')
         sh1 = wb["A1 KTRA-IND"]
         sh2 = wb["A2 GANJ"]
         sh3 = wb["A3 BIDUPUR"]
         sh4 = wb['A4 MARAI']
-        print('File successfully connected.\n')
+        print(Color.BLUE + Color.BOLD + 'File successfully connected.\n' + Color.END)
     except:
-        print("File path is invalid!")
+        print(Color.RED + "ERROR: File path is invalid!" + Color.END)
         exit()
 
     date_order_sheet = sh1["L1"].value
     print("Excel Order Sheet Date is ", date_order_sheet, '\n')
 
-    start = input("\nStart Taking Order? [Y/N]\n")
+    start = input("Start Taking Order? [Y/N]")
     if (start == "Y") or (start == "y"):
         print("Let's Start taking orders,", name, "!")
     else:
         print('Bye,', name, '!')
         exit()
 
-    sh = int(input("[1] 'A1 KTRA-IND'\n"
-                   "[2] 'A2 GANJ'\n"
-                   "[3] 'A3 BIDUPUR'\n"
-                   "[4] 'A4 MARAI'\n"
-                   "Enter 1 or 2 or 3 or 4:\n"))
+    print(Color.BOLD+"[1]+ 'A1 KTRA-IND'\n"
+          "[2] 'A2 GANJ'\n"
+          "[3] 'A3 BIDUPUR'\n"
+          "[4] 'A4 MARAI'"+Color.END)
+    sh = int(input("Enter 1 or 2 or 3 or 4:\n"))
 
     row = 5
     column = 1
@@ -161,7 +174,7 @@ if (ans2 == "Y") or (ans2 == "y"):
             print(count)
             print("Store: ", sh2.cell(row, 2).value)
             print("Call: ", sh2.cell(row, 3).value, "\n")
-           #print(tabulate(sh2_list, headers=["Item", "Quantity", "Selling Price"], numalign="left"))
+            # print(tabulate(sh2_list, headers=["Item", "Quantity", "Selling Price"], numalign="left"))
             count += 1
             row += 1
     if sh == 3:
